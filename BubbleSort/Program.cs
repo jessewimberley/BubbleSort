28d400f60e5b580184c10b983e78e31421d01bfe
Jesse Wimberley <jesse.wimberley@gmail.com>
2020-12-13 12:52:52 -0800

    Add project files.

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 5, 1, 2, 3, 4, 6, 8, 9, 7 };
+            int temp;
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] > nums[i + 1])
+                {
+                    temp = nums[i];
+                    nums[i] = nums[i + 1];
+                    nums[i + 1] = temp;
+
+                }
+            }
+    } }
+}
