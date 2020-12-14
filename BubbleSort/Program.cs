7651cc6d1c371d51c342e2ca5f3ba4912af0d28d
Jesse Wimberley <jesse.wimberley@gmail.com>
2020-12-14 08:24:32 -0800

    Multi-pass

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -6,7 +6,7 @@ namespace BubbleSort
     {
         static void Main(string[] args)
         {
-            int[] nums = { 5, 1, 2, 3, 4, 6, 8, 9, 7 };
+            int[] nums = { 5, 1, 4, 3, 9, 6, 8, 2, 7 };
             int temp;
             for (int j = 0; j < nums.Length - 1; j++)
             {
@@ -21,8 +21,14 @@ namespace BubbleSort
                         canary = 1;
                     }
                 }
+                for (int k = 0; k < nums.Length - 1; k++)
+                {
+                    Console.Write(nums[k]);
+                }
+                Console.WriteLine();
                 if(canary == 0) { break; }
             }
         }
     }
 }
+// This code does an extra pass after the array is sorted.
